760d61feead6a3e9d70e9ffa59697c9c2ac9ed18
Inga Viitanen <ingaa134@gmail.com>
2024-09-26 16:48:08 +0200

    delete todo

--- todo-app/src/api.ts
+++ todo-app/src/api.ts
@@ -3,6 +3,7 @@
  */
 import axios from "axios";
 import type { Todo } from "./api.types";
+import { getTodosAndRender } from "./main";
 
 const baseUrl = import.meta.env.VITE_API_BASEURL as string;
 
@@ -19,11 +20,30 @@ export const getTodos = async () => {
  */
 export const createTodo = async (todo: Todo) => {
 	// Send a POST-request to http://localhost:3000/todos with the contents of `todo` as body
-    const data = await axios.post(baseUrl + '/todos', todo, {
+    const response = await axios.post(baseUrl + '/todos', todo, {
         headers: {
           'Content-Type': 'application/json'
         }
       })
 
-      console.log(data.data)
+      console.log(response.data)
+}
+
+// Mark todo finished or unfinished
+export const updateTodo = async (todo: Todo, id: number) => {
+    // Send a PATCH-request to update completed state
+    const response = await axios.patch(baseUrl + `/todos/${id}`, {
+        completed: true
+    })
+
+    // console.log(response)
+}
+
+export const deleteTodo = async (id: string) => {
+        console.log('hej from deleteTodo', id)
+        const response = await axios.delete(baseUrl + `/todos/${id}`)
+        if(response.status === 200) {
+            getTodosAndRender()
+        }
+        console.log(response)
 }
--- todo-app/src/main.ts
+++ todo-app/src/main.ts
@@ -1,5 +1,5 @@
 import './style.css'
-import { getTodos, createTodo } from "./api.ts";
+import { getTodos, createTodo, updateTodo, deleteTodo } from "./api.ts";
 import type { Todo } from "./api.types.ts";
 
 // Get referenses
@@ -10,61 +10,93 @@ const newTodoFormEl = document.querySelector<HTMLFormElement>("#new-todo-form")!
 let todos: Todo[] = [];
 
 // Get todos from API and render them
-const getTodosAndRender = async () => {
-	// Get todos from server and update local copy
-	todos = await getTodos();
+export const getTodosAndRender = async () => {
+  // Get todos from server and update local copy
+  todos = await getTodos();
 
-	// Render dem todos
-	renderTodos();
+  // Render them todos
+  renderTodos();
 
-  // console.log('getTodosAndRender', todos)
+  deleteTask()
+ 
+  
 }
 
 //Render todos to DOM
 const renderTodos = () => {
-	todosEl.innerHTML = todos
-		.map(todo =>
-			`<li class="list-group-item d-flex justify-content-between align-items-center" data-todo-id="${todo.id}">
+  // onchange=${updateTodo(todo, todo.id)}
+  todosEl.innerHTML = todos
+    .map((todo) => {
+      return `<li class="list-group-item d-flex justify-content-between align-items-center" id="list" data-todo-id="${todo.id}">
 				<span class="todo-item">
-					<input type="checkbox" class="me-2" ${todo.completed ? "checked" : ""} />
+					<input type="checkbox" id="check" class="me-2" ${todo.completed ? "checked" : ""}  />
 					<span class="todo-title">${todo.title}</span>
 				</span>
 				<span class="todo-actions">
 					<button class="btn btn-warning">Edit</button>
-					<button class="btn btn-danger">Delete</button>
+					<button class="delete" id="deleteBtn" >Delete</button>
 				</span>
 			</li>`
-		)
-		.join("");
-
-    // console.log('renderTodos', todos)
+    })
+    .join("")
 }
 
 /**
  * Listen for new todo form being submitted
  */
 newTodoFormEl.addEventListener("submit", (e) => {
-	e.preventDefault();
+  e.preventDefault();
 
-	const newTodoTitleEl = document.querySelector<HTMLInputElement>("#new-todo-title")!;
+  const newTodoTitleEl = document.querySelector<HTMLInputElement>("#new-todo-title")!;
 
   const todo = {
     id: Math.random(), //installing uuid feels like overkill for this project
-		title: newTodoTitleEl.value,
-		completed: false
-	}
+    title: newTodoTitleEl.value,
+    completed: false
+  }
 
-	// Create the todo in the API (and wait for the request to be completed)
+  // Create the todo in the API (and wait for the request to be completed)
   createTodo(todo)
 
-	// Get todos from API (which will include the newly created todo) and re-render the list
-	getTodosAndRender();
+  // Get todos from API (which will include the newly created todo) and re-render the list
+  getTodosAndRender();
 
-	// Clear input field
-	newTodoTitleEl.value = "";
+  // Clear input field
+  newTodoTitleEl.value = "";
 
-	console.log("GREAT SUCCESS!", todos);
+  console.log("GREAT SUCCESS!", todos);
 });
+
+const deleteTask = () => {
+   // Get to all delete button elements
+   const deleteBtn = document.querySelectorAll<HTMLButtonElement>("#deleteBtn")
+
+   // Loop over each delete button and add click event to each btn
+   deleteBtn.forEach(btn => btn.addEventListener("click", () => {
+     // get data set from parent list element
+     const id = btn.parentElement?.parentElement?.getAttribute('data-todo-id')
+
+     //since the ul list can be empty we need to check that the id is a string::Type Narrowing
+     if(id) {
+       deleteTodo(id)
+     }
+    }))
+
+    // render list to update UI -----FIX gets stuck in infinity loop
+    // getTodosAndRender()
+}
+
+// const checkInputEl = (<HTMLInputElement>document.getElementById('#check'));
+
+// console.log(checkInputEl)
+
+// checkInputEl?.addEventListener("change", () => {
+//   // e.preventDefault();
+//   if(checkInputEl.checked){
+//     console.log(checkInputEl.getAttribute('data-todo-id'))
+//   }
+//   // updateTodo(todo) 
+// })
 
 // Get the todos from the API and *then* render initial list of todos
 getTodosAndRender();
